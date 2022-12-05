6ce73a195877c30a5130c1ea85464640dafdfdef
kerokero014 <101151724+kerokero014@users.noreply.github.com>
2022-12-05 08:56:28 -0700

    actor and ball dot CS

---
+++ Casting/Ball.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using cse210_batter_csharp.Casting;
+
+namespace cse210_batter_csharp.Casting
+{
+    /// <summary>
+    /// The base class of all other actions.
+    /// </summary>
+    public class Ball : Actor
+    {
+       public Ball(int x, int y)
+        {
+            SetImage(Constants.IMAGE_BALL);
+            SetHeight(Constants.BALL_HEIGHT);
+            SetWidth(Constants.BALL_WIDTH);
+
+            int _x = x;
+            int _y = y;
+            Point position = new Point(_x, _y);
+            SetPosition(position);
+            
+
+            SetVelocity(new Point(5, 5));
+        }
+       
+    }
+}
